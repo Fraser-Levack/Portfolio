b11bc3455c506d785910e337967e0618b1b35af9
Fraser W Levack <2777042L@student.gla.ac.uk>
2025-02-23 20:26:16 +0000

    fix - screen size fixes

--- src/components/ParallaxHill.tsx
+++ src/components/ParallaxHill.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSpring, animated } from '@react-spring/web';
 
 interface ParallaxHillProps {
@@ -9,19 +9,35 @@ interface ParallaxHillProps {
 }
 
 const ParallaxHill: React.FC<ParallaxHillProps> = ({ src, alt, className, speed }) => {
+    const [scale, setScale] = useState(1);
+
+    useEffect(() => {
+        const updateScale = () => {
+            if (window.innerWidth > 1366) {
+                setScale(1.2);
+            } else {
+                setScale(1);
+            }
+        };
+
+        updateScale();
+        window.addEventListener('resize', updateScale);
+        return () => window.removeEventListener('resize', updateScale);
+    }, []);
+
     const [props, set] = useSpring(() => ({
-        transform: 'translateY(0px)',
+        transform: `translateY(0px) scale(${scale})`,
     }));
 
     useEffect(() => {
         const handleScroll = () => {
             const offset = window.scrollY * speed;
-            set({ transform: `translateY(${offset}px)` });
+            set({ transform: `translateY(${offset}px) scale(${scale})` });
         };
 
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-    }, [speed, set]);
+    }, [speed, set, scale]);
 
     return (
         <animated.img
